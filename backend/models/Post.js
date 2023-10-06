ca91b63adb0e79b12a2b7db8037954aaa7c15c87
umesh <umesharma.us11@gmail.com>
2023-10-06 22:20:07 +0530

    Added image upload functionality in the post route using cloudinary an multer

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -14,7 +14,14 @@ const postSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  likes: Number,
+  cloudinaryId: {
+    type: String,
+    require: true,
+  },
+  likes: {
+    type: Number,
+    require: true,
+  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,10 +2,16 @@ const express = require('express');
 const postRouter = express.Router();
 const postController = require('../controller/posts');
 const { userExtractor } = require('../middleware/auth');
+const upload = require('../middleware/multer');
 
 postRouter.get('/:id', postController.getSinglePost);
 
-postRouter.post('/createPost', userExtractor, postController.createPost);
+postRouter.post(
+  '/createPost',
+  userExtractor,
+  upload.single('file'),
+  postController.createPost
+);
 
 postRouter.delete('/deletePost/:id', userExtractor, postController.deletePost);
 
