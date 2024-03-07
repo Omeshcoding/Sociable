5c6f150523b1449a3e4b671b81dfb0d81f71d6b7
umesh <umesharma.us11@gmail.com>
2024-03-07 16:59:44 +0530

    Added configuration for the production and development environment

--- frontend/src/services/login.js
+++ frontend/src/services/login.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:4002/login';
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 const login = async (credentials) => {
-  const response = await axios.post(baseUrl, credentials);
+  const response = await axios.post(`${baseUrl}/login`, credentials);
 
   return response.data;
 };
--- frontend/src/services/newUser.js
+++ frontend/src/services/newUser.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:4002/signup';
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 const register = async (credentials) => {
-  const response = await axios.post(baseUrl, credentials);
+  const response = await axios.post(`${baseUrl}/signup`, credentials);
   return response.data;
 };
 export default { register };
