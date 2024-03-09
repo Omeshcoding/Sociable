1b6e501a3723fbabcc5a284793c4ad934d3e1891
umesh <umesharma.us11@gmail.com>
2024-03-09 14:33:04 +0530

    Fixed some styling issues with header

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="top-0 sticky bg-background-3 py-4 px-8 font-bold  text-center  shadow-lg z-30">
+    <header className="top-0 sticky bg-background-3 py-4 lg:pl-24 font-bold  text-center lg:text-left shadow-lg z-30">
       <h1 className=" text-secondary-3 text-4xl ">Sociable</h1>
     </header>
   );
